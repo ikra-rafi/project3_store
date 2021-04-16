afef6e193be75e46e2a116f59b69726dac3fb3a8
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-04-16 00:03:57 -0400

    initial shopping cart page

--- client/src/pages/test/TempProducts.js
+++ client/src/pages/test/TempProducts.js
@@ -7,77 +7,77 @@ import {Row, Container} from "../../components/Test/Grid";
 function Temp () {
     // set up state for saved books and a trigger
     const [products, setProducts]= useState([]);
-    const [trigger, setTrigger] = useState("1");
-    var john = [];
+    // const [trigger, setTrigger] = useState("1");
+    // var john = [];
 
-    const storeProducts = [
-        { name: "cinnamon",
-          description: "sweet taste ground bark for baking",
-          packaging: [
-              { size: "4oz bottle",
-                price: "5.99",
-                quantity: 5
-              },
-              { size: "16 oz flatpack",
-                price: "9.99",
-                quantity: 10
-              }],
-          healthbenefit: "www.cnn.com",
-          picLink: "./images/cinnamon",
-          historyDetails: "Cinnamon hails from 5 regions of the world",
-          family: {
-              baking: true,
-              grilling: false,
-              seasoning: false,
-              extract: false,
-              teas: false
-          },
-          region: {
-              india: false,
-              asia: true,
-              carribean: false,
-              middleEast: false,
-              african: false,
-              latinAmerica: false,
-              europe: false
-          },
-          productID: "Cinn0001",
-          ratings: [{ stars: 4, stars: 5, stars: 3, stars: 4}]
-        },
-        { name: "paprika",
-          description: "sweet red pepper dried and ground",
-          packaging: [
-              { size: "4oz bottle",
-                price: "3.99",
-                quantity: 2
-              },
-              { size: "16 oz flatpack",
-                price: "5.99",
-                quantity: 3
-              }],
-          healthbenefit: "www.bbc.com",
-          picLink: "./images/paprika",
-          historyDetails: "Paprika comes from 2 regions - Hungary or Spain.   It comes in sweet, hot & smoked.",
-          family: {
-              baking: true,
-              grilling: true,
-              seasoning: true,
-              extract: false,
-              teas: false
-          },
-          region: {
-              india: false,
-              asia: false,
-              carribean: false,
-              middleEast: false,
-              african: false,
-              latinAmerica: false,
-              europe: true
-          },
-          productID: "Paprika0025",
-          ratings: [{ stars: 2, stars: 2, stars: 5, stars: 3}]
-        }
-    ]
+    // const storeProducts = [
+    //     { name: "cinnamon",
+    //       description: "sweet taste ground bark for baking",
+    //       packaging: [
+    //           { size: "4oz bottle",
+    //             price: "5.99",
+    //             quantity: 5
+    //           },
+    //           { size: "16 oz flatpack",
+    //             price: "9.99",
+    //             quantity: 10
+    //           }],
+    //       healthbenefit: "www.cnn.com",
+    //       picLink: "./images/cinnamon",
+    //       historyDetails: "Cinnamon hails from 5 regions of the world",
+    //       family: {
+    //           baking: true,
+    //           grilling: false,
+    //           seasoning: false,
+    //           extract: false,
+    //           teas: false
+    //       },
+    //       region: {
+    //           india: false,
+    //           asia: true,
+    //           carribean: false,
+    //           middleEast: false,
+    //           african: false,
+    //           latinAmerica: false,
+    //           europe: false
+    //       },
+    //       productID: "Cinn0001",
+    //       ratings: [{ stars: 4, stars: 5, stars: 3, stars: 4}]
+    //     },
+    //     { name: "paprika",
+    //       description: "sweet red pepper dried and ground",
+    //       packaging: [
+    //           { size: "4oz bottle",
+    //             price: "3.99",
+    //             quantity: 2
+    //           },
+    //           { size: "16 oz flatpack",
+    //             price: "5.99",
+    //             quantity: 3
+    //           }],
+    //       healthbenefit: "www.bbc.com",
+    //       picLink: "./images/paprika",
+    //       historyDetails: "Paprika comes from 2 regions - Hungary or Spain.   It comes in sweet, hot & smoked.",
+    //       family: {
+    //           baking: true,
+    //           grilling: true,
+    //           seasoning: true,
+    //           extract: false,
+    //           teas: false
+    //       },
+    //       region: {
+    //           india: false,
+    //           asia: false,
+    //           carribean: false,
+    //           middleEast: false,
+    //           african: false,
+    //           latinAmerica: false,
+    //           europe: true
+    //       },
+    //       productID: "Paprika0025",
+    //       ratings: [{ stars: 2, stars: 2, stars: 5, stars: 3}]
+    //     }
+    // ]
 
     // Load all saved books and store them with setBooks
     useEffect(() => {
@@ -96,27 +96,27 @@ function Temp () {
     // };
 
 
-    function saveProducts(storeProducts) {
-        console.log("inside saveProducts");
-        console.log(storeProducts);
-        API.saveProducts(storeProducts)
-        // .then(res => API.getProducts()
-        //                 .then(res=> { console.log("products"); console.log(res.data); john =res.data; console.log("john=" + john)
-        //                 console.log("length = " + john.length)
-        //                 setProducts(john)})
-        //                 .catch(err => console.log(err))
+//     function saveProducts(storeProducts) {
+//         console.log("inside saveProducts");
+//         console.log(storeProducts);
+//         API.saveProducts(storeProducts)
+//         // .then(res => API.getProducts()
+//         //                 .then(res=> { console.log("products"); console.log(res.data); john =res.data; console.log("john=" + john)
+//         //                 console.log("length = " + john.length)
+//         //                 setProducts(john)})
+//         //                 .catch(err => console.log(err))
             
-//            )
-        .catch(err => console.log(err));
-    }
+// //            )
+//         .catch(err => console.log(err));
+//     }
 
-    function handleBtnClick(event) {
-        API.saveProducts(storeProducts)
-           .then(res=> { console.log("products"); console.log(res.data); john =res.data; console.log("john=" + john)
-                        console.log("length = " + john.length)
-                        setProducts(john)})
-            .catch(err => console.log(err));
-    }
+    // function handleBtnClick(event) {
+    //     API.saveProducts(storeProducts)
+    //        .then(res=> { console.log("products"); console.log(res.data); john =res.data; console.log("john=" + john)
+    //                     console.log("length = " + john.length)
+    //                     setProducts(john)})
+    //         .catch(err => console.log(err));
+    // }
 
     function getProducts() {
         API.getProducts()
@@ -130,7 +130,7 @@ function Temp () {
     // return the rendered saved books page
     return (
             <Container fluid>
-                <button className="btn myButton buttonMargin" style={{ fontSize: "20px"}} onClick={handleBtnClick}><strong>Products</strong></button>
+                <button className="btn myButton buttonMargin" style={{ fontSize: "20px"}} ><strong>Products</strong></button>
                 <Row>
                 <div className="container-fluid containerColor marginBottomCont">
                     {products.length ? (
--- client/src/pages/test/TempRecipes.js
+++ client/src/pages/test/TempRecipes.js
@@ -7,59 +7,62 @@ import {Row, Container} from "../../components/Test/Grid";
 function TempRecipes () {
     // set up state for saved books and a trigger
     const [recipes, setRecipes]= useState([]);
-    const [trigger, setTrigger] = useState("1");
 
-    const storeRecipes = [
-    {   recipeDesc: "Lasagna",
-        recipeIngredients: [
-          { ingredient: "noodles"},
-          { ingredient: "sauce"},
-          { ingredient: "oregano"},
-          { ingredient: "garlic"},
-          { ingredient: "hamburger meat"},
-          { ingredient: "cheese"}
-        ],
-        recipeSteps: [
-          { step: "1. Brown hamburger meat."},
-          { step: "2. Cook noodles per package instructions."},
-          { step: "3. Simmer sauce, garlice and oregano until heated."},
-          { step: "4. Combine meat and sauce."},
-          { step: "5. Spread layer of sauce in pan."},
-          { step: "6. Top with noodles."},
-          { step: "7. Add cheese."},
-          { step: "8. Add more sauce."},
-          { step: "9. Repeat for more layers."},
-          { step: "10. Bake for 1 hr at 350deg."}
-        ],
-        productID: "LASAGNA0001"
-    },
-    {
-      recipeDesc: "cake",
-      recipeIngredients: [
-        { ingredient: "cake mix"},
-        { ingredient: "2 eggs" },
-        { ingredient: "oil"},
-        { ingredient: "water"}
-      ],
-      recipeSteps: [
-        { step: "1. Combine eggs, cake mix, water, oil in bowl"},
-        { step: "2. Beat until smooth and no lumps."},
-        { step: "3. Pour batter into greased pan."},
-        { step: "4. Bake for 35min at 350deg."},
-      ],
-      productID: "CAKE00012"    
-    }];
+    // const storeRecipes = [
+    // {   recipeDesc: "Lasagna",
+    //     recipeIngredients: [
+    //       { ingredient: "noodles"},
+    //       { ingredient: "sauce"},
+    //       { ingredient: "oregano"},
+    //       { ingredient: "garlic"},
+    //       { ingredient: "hamburger meat"},
+    //       { ingredient: "cheese"}
+    //     ],
+    //     recipeSteps: [
+    //       { step: "1. Brown hamburger meat."},
+    //       { step: "2. Cook noodles per package instructions."},
+    //       { step: "3. Simmer sauce, garlice and oregano until heated."},
+    //       { step: "4. Combine meat and sauce."},
+    //       { step: "5. Spread layer of sauce in pan."},
+    //       { step: "6. Top with noodles."},
+    //       { step: "7. Add cheese."},
+    //       { step: "8. Add more sauce."},
+    //       { step: "9. Repeat for more layers."},
+    //       { step: "10. Bake for 1 hr at 350deg."}
+    //     ],
+    //     productID: "LASAGNA0001"
+    // },
+    // {
+    //   recipeDesc: "cake",
+    //   recipeIngredients: [
+    //     { ingredient: "cake mix"},
+    //     { ingredient: "2 eggs" },
+    //     { ingredient: "oil"},
+    //     { ingredient: "water"}
+    //   ],
+    //   recipeSteps: [
+    //     { step: "1. Combine eggs, cake mix, water, oil in bowl"},
+    //     { step: "2. Beat until smooth and no lumps."},
+    //     { step: "3. Pour batter into greased pan."},
+    //     { step: "4. Bake for 35min at 350deg."},
+    //   ],
+    //   productID: "CAKE00012"    
+    // }];
 
     // Load all saved books and store them with setBooks
     // useEffect(() => {
     //     console.log("in useEffect orders");
     //     saveOrders(storeOrders);
     //   })
+    // useEffect(() => {
+    //     console.log("recipes Effect");
+    //     getRecipes();
+    // }, [])
+
     useEffect(() => {
         console.log("recipes Effect");
         getRecipes();
     }, [])
-
     // function to retrieve the saved books from database
     // function loadSavedBooks() {
     //     API.getBooks()
@@ -71,35 +74,32 @@ function TempRecipes () {
     // };
 
 
-    function saveRecipes(storeRecipes) {
-        console.log("inside recipes");
-        console.log(storeRecipes);
-        API.saveRecipes(storeRecipes)
-        .then(res => API.getRecipes()
-                        .then(res=> { console.log("recipes"); console.log(res.data); recipes =res.data})
-                        .catch(err => console.log(err))
+    // function saveRecipes(storeRecipes) {
+    //     console.log("inside recipes");
+    //     console.log(storeRecipes);
+    //     API.saveRecipes(storeRecipes)
+    //     .then(res => API.getRecipes()
+    //                     .then(res=> { console.log("recipes"); console.log(res.data); recipes =res.data})
+    //                     .catch(err => console.log(err))
             
-            )
-        .catch(err => console.log(err));
-    }
+    //         )
+    //     .catch(err => console.log(err));
+    // }
 
-    function handleRecipeBtnClick(event) {
-        API.saveRecipes(storeRecipes)
-            .then(res => {
-                console.log("recipes");
-                console.log(res.data);
-                setRecipes(res.data);
-            })
-            .catch(err => console.log(err));
-    }
+    // function handleRecipeBtnClick(event) {
+    //     API.saveRecipes(storeRecipes)
+    //         .then(res => {
+    //             console.log("recipes");
+    //             console.log(res.data);
+    //             setRecipes(res.data);
+    //         })
+    //         .catch(err => console.log(err));
+    // }
 
     function getRecipes() {
         API.getRecipes()
         .then(res=> {
-            console.log("recipes = " + res.data);
-            console.log(res.data[0])
             setRecipes(res.data);
-            console.log(recipes.length)
         })
         .catch(err => console.log(err))
     }
@@ -107,7 +107,7 @@ function TempRecipes () {
     // return the rendered saved books page
     return (
             <Container fluid>
-                <button className="btn myButton buttonMargin" style={{ fontSize: "20px"}} onClick={handleRecipeBtnClick}><strong>Recipes</strong></button>
+                <button className="btn myButton buttonMargin" style={{ fontSize: "20px"}} ><strong>Recipes</strong></button>
                 <Row>
                 <div className="container-fluid containerColor marginBottomCont">
                     {recipes.length ? (
