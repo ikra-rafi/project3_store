587967931466cd05ccfc5e00ba6198909e0308cc
Luna Shuqair <luna.shuqair89@gmail.com>
2021-04-17 10:39:50 -0400

    Login

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,13 +1,61 @@
 import React from "react";
-//import { useStoreContext } from "../../utils/GlobalState";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+
 import "./style.css";
 
-function LoginForm() {
 
+
+
+function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+  const intialValues = {
+    
+    password: "",
+    email: "",
+    
+  };
 
   return (
-   <div></div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+     
+
+      
+      <label htmlFor="email">Email</label>
+      <input
+        defaultValue={intialValues.email}
+        placeholder="name@gmail.com"
+        type="email"
+        {...register("email")}
+      />
+      
+      <label htmlFor="password">Password</label>
+      <input
+        defaultValue={intialValues.password}
+        placeholder="password"
+        {...register("password", {
+          validate: (value) => value.length > 6
+        })}
+      />
+      {errors. password&& <p>Your Password is less than 6 characters</p>}
+
+      
+      <input type="submit" />
+    </form>
   );
 }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<LoginForm/>, rootElement);
+
+  
 
 export default LoginForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,14 @@ import ContactUsForm from "../components/ContactUsForm.js";
 import Container from "../components/Container";
 import ProductsTable from "../components/ProductsTable";
 
+
 function Admin() {
   return (
     <div>
       <Container>
         <ProductsTable />
         <ContactUsForm />
+   
       </Container>
 </div>
   );
