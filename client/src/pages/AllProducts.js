5de7682cba562e691ccb5c3e9bf53247c8c78c4f
Ashley Stith <ashleyc.stith@gmail.com>
2021-04-29 00:40:29 -0400

    Minor update to scroll to top

--- client/src/pages/AllProducts.js
+++ client/src/pages/AllProducts.js
@@ -13,7 +13,7 @@ function AllProducts() {
 
 // When the component mounts, a call will be made to get load products.
 useEffect(() => {
-  window.scrollTo(0, 0);
+  // window.scrollTo(0, 0);
   document.body.scrollTop = 0;
   loadProducts();
 }, []);
